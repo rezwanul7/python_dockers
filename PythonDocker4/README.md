19cdf08001ae9b69b640759c424a954c2f093b24
Rezwanul Islam <rezwanul7@gmail.com>
2023-07-14 19:23:42 +0600

    add a PythonDocker Project

---
+++ PythonDocker4/README.md
@@ -0,0 +1,4 @@
+# Python App Docker Containers
+
+Package to install: NumPy
+Package Installations Using: pip [directly]
---
+++ PythonDocker4/app.py
@@ -0,0 +1,8 @@
+from definitions import HELLO_WORLD
+import numpy as np
+
+if __name__ == "__main__":
+    print(HELLO_WORLD)
+    arr = np.array([1, 2, 3, 4, 5])
+    print(arr)
+    print(type(arr))
